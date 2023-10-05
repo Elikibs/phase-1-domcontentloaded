284fa0d86b1add67b1cbce09179be11c74a1dcbc
Elisha <elishakibet67@gmail.com>
2023-10-05 15:57:18 +0300

    Use of DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const textInParagraph = document.getElementById("text");
+    textInParagraph.textContent = "This is really cool!";
+});
